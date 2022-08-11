3747ffc6c72be70ba77c0673549d3ad8a152d614
 Mekala Vamsi Krishna <68817698+mekala-vamsi-krishna@users.noreply.github.com>
2022-08-11 15:10:19 +0530

    Add files via upload

---
+++ 12. Tipsy-iOS13/Tipsy/Controllers/ResultViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Mekala Vamsi Krishna on 09/07/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    var result = "0.0"
+    var tip = 10
+    var split = 2
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        totalLabel.text = result
+        settingsLabel.text = "Split between \(split) people, with \(tip)% tip."
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
